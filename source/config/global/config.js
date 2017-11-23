d6e5c4c0d17095e44aa198adfa9015df6ca96baa
goldea <goldea.goldea@gmail.com>
2017-11-23 16:51:06 +0300

    Заменил перевод для Timeline

--- source/config/global/config.js
+++ source/config/global/config.js
@@ -1,218 +1,301 @@
-window.config = Object.assign(window.config || {}, {
-  sale: '0x3076d20cef23cf934fc6f857eb97e2baa63fc9fb',
-  common: {
-    contactEmail: 'support@goldea.io',
-    termsOfServiceURL: '',
-  },
-  faq: {
-    entries: [
-      {
-        question: 'How is gold mining?',
-        answer: 'Environmentally friendly gravitational technique based on completely mobile plants.',
-      },
-      {
-        question: 'Do you have a JORC?',
-        answer: 'The JORC is used primarily by public listed large mining companies which are interested in huge hard rock gold deposits. We are small scale mining company like our gold mining licence states and our mining strategy doesn’t require JORC.',
-      },
-      {
-        question: 'In what way did geological exploration work?',
-        answer: 'As we are small scale mining company we did standard exploration works which confirmed 10 tons gold reserves and tested results in independent laboratories in Canada and Austria. All exploration works and its interpretation you can find in documents deposited in our personal cabinet.',
-      },
-      {
-        question: 'Inventory Counts?',
-        answer: '10 tons',
-      },
-      {
-        question: 'What will the technique be used for gold mining?',
-        answer: 'Environmentally friendly gravitational technique based on completely mobile plants.',
-      },
-      {
-        question: 'When will the plant be built?',
-        answer: '8 months after raised funds',
-      },
-      {
-        question: 'Is it possible to exchange a token for gold?',
-        answer: 'Yes if owner of tokens fills out KYC and take over all delivery costs.',
-      },
-      {
-        question: 'Earlier there was a return ransom, why was it removed?',
-        answer: 'Buy back will start 1 month after production starts with initially 10% of tokens in market',
-      },
-      {
-        question: 'Investors who invested in the Pre-ICO. Will there be a redemption for them?',
-        answer: 'Yes',
-      },
-      {
-        question: 'You had an agreement with the Swiss factory ... what now with him?',
-        answer: 'We have preliminary agreement . We are in process of incorporating GOLDEA company in Dubai and after incorporation will sign agreement with Swiss Bullion Company.  Gold is the only product in world for which you don’t need to look for buyer so there is zero risk and no worry. We have many buyers waiting for us.',
-      },
-      {
-        question: 'When will the 1.2.3.4 round? What date do you plan to complete the ICO?',
-        answer: '1st from 15 November, second from 15th December  till 15 January, third from 15 January till 15 February and fourth from 15 February till 15 March',
-      },
-      {
-        question: 'How much do you need to raise funds to start building a factory?',
-        answer: '1.000.000 USD',
-      },
-      {
-        question: 'What is Feasibility studio and what languages will be translated?',
-        answer: 'It is advanced version of business plan and for beggining will be on english and later will be translated',
-      },
-      {
-        question: 'Why ICO in 4 rounds?',
-        answer: 'In order to provide investors full transparency of funds usage and minimize all risks. Each cent will be justified with video content through our media contents.',
-      },
-      {
-        question: 'How long will the plant be built after the fundraising? It is desirable to have a time range with justification of the terms.',
-        answer: 'From 6-8 months after completion of fundraising. Detailed description you can find in Feasibility Study (Business Plan)',
-      },
-      {
-        question: 'How will the profit increase from extraction affect the price of the token?',
-        answer: 'We have limited amount of tokens and profit will grow unlimited. So token will grow as profit of company grows. We are tokenizing profit from gold production.',
-      },
-      {
-        question: 'Infrastructure - what you need to fail to start production?',
-        answer: 'Roads, camp facilities for workers and staff, security system, water tanks, electricity generator and installations, parking.',
-      },
-    ],
-    documents: [
-      {
-        url: '',
-        previewUrl: 'http://urhelp.guru/images/27579/dokumenty-na-prava-sobstvennosti.jpg',
-      },
-      {
-        url: '',
-        previewUrl: 'http://urhelp.guru/images/27579/dokumenty-na-prava-sobstvennosti.jpg',
-      },
-      {
-        url: '',
-        previewUrl: 'http://urhelp.guru/images/27579/dokumenty-na-prava-sobstvennosti.jpg',
-      },
-      {
-        url: '',
-        previewUrl: 'http://urhelp.guru/images/27579/dokumenty-na-prava-sobstvennosti.jpg',
-      },
-    ],
-  },
-  saleTimeline: [
-    {
-      title: '15.11.2017 (Базовая цена 12$):',
-      steps: [
-        {
-          date: '5.11.2017 – 21.11.2017',
-          text: 'Первая неделя скидка',
-          percent: '15%',
-          isActive: true,
-        },
-        {
-          date: '22.11.2017 – 28.11.2017',
-          text: 'Вторая неделя скидка',
-          percent: '10%',
-          isActive: false,
-        },
-        {
-          date: '29.11.2017 – 05.12.2017',
-          text: 'Третья неделя скидка',
-          percent: '5%',
-          isActive: false,
-        },
-        {
-          date: '06.12.2017 – 14.12.2017',
-          text: 'Четвертая неделя скидка',
-          percent: '0%',
-          isActive: false,
-        },
-      ],
-    },
+/* eslint-disable */
+var common = {
+  contactEmail: 'support@goldea.io',
+  termsOfServiceURL: '',
+};
 
+var faq = {
+  entries: [
     {
-      title: '15.12.2017 (Базовая цена 15$):',
-      steps: [
-        {
-          date: '15.12.2017 – 21.12.2017',
-          text: 'Первая неделя скидка',
-          percent: '15%',
-          isActive: false,
-        },
-        {
-          date: '22.12.2017 – 28.12.2017',
-          text: 'Вторая неделя скидка',
-          percent: '10%',
-          isActive: false,
-        },
-        {
-          date: '29.12.2017 – 04.01.2018',
-          text: 'Третья неделя скидка',
-          percent: '5%',
-          isActive: false,
-        },
-        {
-          date: '05.01.2018 – 14.01.2018',
-          text: 'Четвертая неделя скидка',
-          percent: '0%',
-          isActive: false,
-        },
-      ],
+      question: 'How is gold mining?',
+      answer: 'Environmentally friendly gravitational technique based on completely mobile plants.',
     },
-
     {
-      title: '15.01.2018 (Базовая цена 18$):',
-      steps: [
-        {
-          date: '15.01.2018 – 21.01.2018',
-          text: 'Первая неделя скидка',
-          percent: '15%',
-          isActive: false,
-        },
-        {
-          date: '22.01.2018 – 28.01.2018',
-          text: 'Вторая неделя скидка',
-          percent: '10%',
-          isActive: false,
-        },
-        {
-          date: '29.01.2018 – 05.02.2018',
-          text: 'Третья неделя скидка',
-          percent: '5%',
-          isActive: false,
-        },
-        {
-          date: '06.02.2018 – 14.02.2018',
-          text: 'Четвертая неделя скидка',
-          percent: '0%',
-          isActive: false,
-        },
-      ],
+      question: 'Do you have a JORC?',
+      answer: 'The JORC is used primarily by public listed large mining companies which are interested in huge hard rock gold deposits. We are small scale mining company like our gold mining licence states and our mining strategy doesn’t require JORC.',
     },
-
     {
-      title: '15.02.2018 (Базовая цена 21$):',
-      steps: [
-        {
-          date: '15.02.2018 – 21.02.2018',
-          text: 'Первая неделя скидка',
-          percent: '15%',
-          isActive: false,
-        },
-        {
-          date: '22.02.2018 – 28.02.2018',
-          text: 'Вторая неделя скидка',
-          percent: '10%',
-          isActive: false,
-        },
-        {
-          date: '01.03.2018 – 07.03.2018',
-          text: 'Третья неделя скидка',
-          percent: '5%',
-          isActive: false,
-        },
-        {
-          date: '08.03.2018 – 14.03.2018',
-          text: 'Четвертая неделя скидка',
-          percent: '0%',
-          isActive: false,
-        },
-      ],
+      question: 'In what way did geological exploration work?',
+      answer: 'As we are small scale mining company we did standard exploration works which confirmed 10 tons gold reserves and tested results in independent laboratories in Canada and Austria. All exploration works and its interpretation you can find in documents deposited in our personal cabinet.',
+    },
+    {
+      question: 'Inventory Counts?',
+      answer: '10 tons',
+    },
+    {
+      question: 'What will the technique be used for gold mining?',
+      answer: 'Environmentally friendly gravitational technique based on completely mobile plants.',
+    },
+    {
+      question: 'When will the plant be built?',
+      answer: '8 months after raised funds',
+    },
+    {
+      question: 'Is it possible to exchange a token for gold?',
+      answer: 'Yes if owner of tokens fills out KYC and take over all delivery costs.',
+    },
+    {
+      question: 'Earlier there was a return ransom, why was it removed?',
+      answer: 'Buy back will start 1 month after production starts with initially 10% of tokens in market',
+    },
+    {
+      question: 'Investors who invested in the Pre-ICO. Will there be a redemption for them?',
+      answer: 'Yes',
+    },
+    {
+      question: 'You had an agreement with the Swiss factory ... what now with him?',
+      answer: 'We have preliminary agreement . We are in process of incorporating GOLDEA company in Dubai and after incorporation will sign agreement with Swiss Bullion Company.  Gold is the only product in world for which you don’t need to look for buyer so there is zero risk and no worry. We have many buyers waiting for us.',
+    },
+    {
+      question: 'When will the 1.2.3.4 round? What date do you plan to complete the ICO?',
+      answer: '1st from 15 November, second from 15th December  till 15 January, third from 15 January till 15 February and fourth from 15 February till 15 March',
+    },
+    {
+      question: 'How much do you need to raise funds to start building a factory?',
+      answer: '1.000.000 USD',
+    },
+    {
+      question: 'What is Feasibility studio and what languages will be translated?',
+      answer: 'It is advanced version of business plan and for beggining will be on english and later will be translated',
+    },
+    {
+      question: 'Why ICO in 4 rounds?',
+      answer: 'In order to provide investors full transparency of funds usage and minimize all risks. Each cent will be justified with video content through our media contents.',
+    },
+    {
+      question: 'How long will the plant be built after the fundraising? It is desirable to have a time range with justification of the terms.',
+      answer: 'From 6-8 months after completion of fundraising. Detailed description you can find in Feasibility Study (Business Plan)',
+    },
+    {
+      question: 'How will the profit increase from extraction affect the price of the token?',
+      answer: 'We have limited amount of tokens and profit will grow unlimited. So token will grow as profit of company grows. We are tokenizing profit from gold production.',
+    },
+    {
+      question: 'Infrastructure - what you need to fail to start production?',
+      answer: 'Roads, camp facilities for workers and staff, security system, water tanks, electricity generator and installations, parking.',
     },
   ],
-});
+  documents: [
+    {
+      url: '',
+      previewUrl: 'http://urhelp.guru/images/27579/dokumenty-na-prava-sobstvennosti.jpg',
+    },
+    {
+      url: '',
+      previewUrl: 'http://urhelp.guru/images/27579/dokumenty-na-prava-sobstvennosti.jpg',
+    },
+    {
+      url: '',
+      previewUrl: 'http://urhelp.guru/images/27579/dokumenty-na-prava-sobstvennosti.jpg',
+    },
+    {
+      url: '',
+      previewUrl: 'http://urhelp.guru/images/27579/dokumenty-na-prava-sobstvennosti.jpg',
+    },
+  ],
+};
+
+var saleTimeline = [
+  {
+    title: '15.11.2017 (Basic price 12$):',
+    steps: [
+      {
+        date: '5.11.2017 – 21.11.2017',
+        text: 'First week sale',
+        percent: '15%',
+        isActive: true,
+      },
+      {
+        date: '22.11.2017 – 28.11.2017',
+        text: 'Second week sale',
+        percent: '10%',
+        isActive: false,
+      },
+      {
+        date: '29.11.2017 – 05.12.2017',
+        text: 'Third week sale',
+        percent: '5%',
+        isActive: false,
+      },
+      {
+        date: '06.12.2017 – 14.12.2017',
+        text: 'Fourth week sale',
+        percent: '0%',
+        isActive: false,
+      },
+    ],
+  },
+
+  {
+    title: '15.12.2017 (Basic price 15$):',
+    steps: [
+      {
+        date: '15.12.2017 – 21.12.2017',
+        text: 'First week sale',
+        percent: '15%',
+        isActive: false,
+      },
+      {
+        date: '22.12.2017 – 28.12.2017',
+        text: 'Second week sale',
+        percent: '10%',
+        isActive: false,
+      },
+      {
+        date: '29.12.2017 – 04.01.2018',
+        text: 'Third week sale',
+        percent: '5%',
+        isActive: false,
+      },
+      {
+        date: '05.01.2018 – 14.01.2018',
+        text: 'Fourth week sale',
+        percent: '0%',
+        isActive: false,
+      },
+    ],
+  },
+
+  {
+    title: '15.01.2018 (Basic price 18$):',
+    steps: [
+      {
+        date: '15.01.2018 – 21.01.2018',
+        text: 'First week sale',
+        percent: '15%',
+        isActive: false,
+      },
+      {
+        date: '22.01.2018 – 28.01.2018',
+        text: 'Second week sale',
+        percent: '10%',
+        isActive: false,
+      },
+      {
+        date: '29.01.2018 – 05.02.2018',
+        text: 'Third week sale',
+        percent: '5%',
+        isActive: false,
+      },
+      {
+        date: '06.02.2018 – 14.02.2018',
+        text: 'Fourth week sale',
+        percent: '0%',
+        isActive: false,
+      },
+    ],
+  },
+
+  {
+    title: '15.02.2018 (Basic price 21$):',
+    steps: [
+      {
+        date: '15.02.2018 – 21.02.2018',
+        text: 'First week sale',
+        percent: '15%',
+        isActive: false,
+      },
+      {
+        date: '22.02.2018 – 28.02.2018',
+        text: 'Second week sale',
+        percent: '10%',
+        isActive: false,
+      },
+      {
+        date: '01.03.2018 – 07.03.2018',
+        text: 'Third week sale',
+        percent: '5%',
+        isActive: false,
+      },
+      {
+        date: '08.03.2018 – 14.03.2018',
+        text: 'Fourth week sale',
+        percent: '0%',
+        isActive: false,
+      },
+    ],
+  },
+];
+
+var texts = {
+  auth: {
+    email: "E-mail",
+    password: "Password",
+    forgotPassword: "Forgot your password?",
+    dontHaveAccount: "Don’t have an account?",
+    alreadyHaveAccount: "Already have an account?",
+    logout: "Logout",
+    signInHeading: "Sign In",
+    signInSubmit: "Sign In",
+    signUpHeading: "Sign Up",
+    signUpSubmit: "Sign Up",
+    signUpSuccess: "Password has been sent to email",
+    forgotPasswordInstruction: "Enter the email address you used when you joined and we’ll send you instructions to reset your password.",
+    forgotPasswordSecurity: "For security reasons, we do NOT store your password. So rest assured that we will never send your password via email.",
+    forgotPasswordSubmit: "Send Reset Instructions",
+    successfulResetTitle: "Reset Instructions Sent",
+    successfulResetAnnotation: "Instructions to reset your password have been sent to you. Please check your email.",
+    createNewPasswordTitle: "Create New Password",
+    newPassword: "New Password",
+    confirmNewPassword: "Confirm New Password",
+    submitNewPassword: "Create New Password",
+    successfulNewPasswordCreation: "New password have been created."
+  },
+  common: {
+    tokenName: "GEA",
+    loading: "Loading",
+    poweredBy: "Powered by",
+    termsOfService: "Terms of Service"
+  },
+  faq: {
+    title: "Frequently Asked Questions"
+  },
+  news: {
+    title: "News",
+    currentStatus: "Current status: ICO stage 1",
+    fundsRaised: "Funds raised: 325ETH 529BTC",
+    projectStatus: "Project status:",
+    goldMinePlantOrdering: "Gold Mine Plant Ordering",
+    infrastructureSetup: "Infrastructure Setup",
+    campConstruction: "Camp Construction",
+    securingHeavyDutyMachinery: "Securing Heavy Duty machinery",
+    goldPropertyPreparation: "Gold Property Preparation",
+    goldMinePlantFinalization: "Gold Mine Plant Finalization"
+  },
+  paymentMethods: {
+    title: "Step 2: Select your payment method",
+    wantToPayWith: "I want to pay with",
+    sendFundsTo: "Send funds to this {{ paymentMethod }} address:",
+    statusesTitle: "Payments statuses",
+    statusFinished: "finished",
+    statusPending: "pending",
+    instructionTitle: "Payment Instruction",
+    instructionText: "After your payment will be completed you will get tokens to the selected ethereum address:"
+  },
+  wallet: {
+    title: "Step 1: Your Ethereum Wallet",
+    annotation: "In order to recieve {{ tokenName }} tokens you need to give your Etherium Wallet Adress.",
+    noticeTitle: "Important",
+    noticeText: "We are not able to send tokens to Exchange Ethereum wallets.",
+    ethereumAddress: "Ethereum address",
+    save: "Save",
+    saved: "Saved"
+  },
+  widgets: {
+    yourWalletBalance: "Your Wallet Balance",
+    tokenPrice: "Token Price",
+    tokensSold: "Tokens Sold",
+    cantFindWhatLookingFor: "Can’t find what are you looking for?",
+    emailUs: "Email Us"
+  },
+};
+
+window.config = {
+  common: common,
+  faq: faq,
+  saleTimeline: saleTimeline,
+  sale: '0x3076d20cef23cf934fc6f857eb97e2baa63fc9fb',
+  texts: texts,
+};
+
+/* eslint-enable */
